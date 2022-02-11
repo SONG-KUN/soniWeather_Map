da3c1569c56fcc669b0c919dc43c541c0f9bb391
Nicolo' Pisanu <nicolopietro.pisanu@gmail.com>
2022-02-11 12:43:17 +0100

    cloudy + wind

--- SoundDesigner/soundGenerator.js
+++ SoundDesigner/soundGenerator.js
@@ -1,5 +1,6 @@
-function sound(weatherParameters) {
+function sound() {
 
+    console.log("sound called")
     /*
     Weather parameters:
     - temperature
@@ -13,19 +14,19 @@ function sound(weatherParameters) {
 
      */
 
-    let wind = 100;
-    let rain = 100;
-    let cloud = 0;
+    //ALL VALUE in range [0;100]
+    let wind = 50;
+    let rain = 0;
+    let cloud = 100;
 
     //MAPPING:
     //lon to freq
-    let freq = scale(weatherParameters[0], -180, 180, 100, 1000)
+    //let freq = scale(weatherParameters[0], -180, 180, 100, 1000)
     //lat to duration
-    let dur = scale(weatherParameters[1], -90, 90, 0.1, 1)
+    //let dur = scale(weatherParameters[1], -90, 90, 0.1, 1)
 
     // duration of the entire event (maybe 1 min?)
     let totalDuration = 60;      //secs
-    let sequence = 5;
     const c = new AudioContext();
     const startTime = c.currentTime;
 
@@ -37,8 +38,8 @@ function sound(weatherParameters) {
     //}
 
     const intervalWind = setInterval(playWind, 10000, wind)
-    const intervalRain = setInterval(playRain, 500, rain)
-    const intervalSky  = setInterval(playSky,  2000, cloud)
+    //const intervalRain = setInterval(playRain, 500, rain)
+    const intervalSky  = setInterval(playSky,  1000, cloud)
 
     function playSky (cloud) {
         const now = c.currentTime;
@@ -47,32 +48,56 @@ function sound(weatherParameters) {
 
         //DURATION PARAMETERS
         //duration between 500 and 4000 ms
-        const dur = randomNumber(500, 4000)/1000;
-        const att = dur/10;
-        const dec = dur/5;
+        const dur = randomNumber(2000, 4000)/1000;
+        const att = dur/2;
+        const dec = dur/2;
 
-        //choose one frequency in the list
-        //CONSONANT
-        const frequencyList = Array(523, 3452, 334, 31, 5346);
-        const freq = frequencyList[Math.floor(Math.random() * frequencyList.length)];
+        const maxAmp = 0.1;
+
+        //choose one frequency in the list depending on the cloud covering:
+        // CONSONANT --> clear
+        // DISSONANT --> cloud
+        // more the cloud more the probability of having dissonance
+
+        const octave = Array (-1, 1, 2);
+        const consonantTransposition = Array(0, 4, 5, 7, 8, 9, 12);
+        const dissonantTransposition = Array(1, 2, 3, 6, 10, 11);
+        let note = 48;
+        let midiTranspose = 0;
+
+        if (cloud < randomNumber(0,100))
+        {
+            //more CLEAR
+            midiTranspose = consonantTransposition[Math.floor(Math.random() * consonantTransposition.length)];
+        }
+        else
+        {
+            //more CLOUDY
+            midiTranspose = dissonantTransposition[Math.floor(Math.random() * dissonantTransposition.length)];
+        }
+
+        midiTranspose = midiTranspose * octave[Math.floor(Math.random() * octave.length)];
 
         //Detuning factor linked to the cloud cover
-        const detune = cloud;
+        //const detune = cloud;
 
-        o1.type = "square";
-        o1.frequency.value = freq;
-        o1.detune.value = detune;
+        o1.type = "sawtooth";
+        note = note + midiTranspose;
+        console.log (midiTranspose, note);
+        o1.frequency.value = noteToFreq(note);
+        //o1.detune.value = detune;
 
         //setting envelope
         g.gain.setValueAtTime(0, now);
-        g.gain.linearRampToValueAtTime(0.1, now+att);
-        g.gain.linearRampToValueAtTime(0.1, now + dur - dec);
+        g.gain.linearRampToValueAtTime(maxAmp, now+att);
+        g.gain.linearRampToValueAtTime(maxAmp, now + dur - dec);
         g.gain.linearRampToValueAtTime(0, now + dur);
 
         //CONNECTION
         o1.connect(g);
-        g.connect(c.destination)
+        g.connect(c.destination);
 
+        o1.start(now);
         o1.stop(now+dur)
 
         if (c.currentTime - startTime > totalDuration)
@@ -102,18 +127,17 @@ function sound(weatherParameters) {
         o1.type = "sine"
         o2.type = "sine"
         o3.type = "sine"
-
-        o1.connect(g);
-        o2.connect(g);
-        o3.connect(g);
-
-        g.connect(c.destination)
-
         //no attack
         g.gain.setValueAtTime(0, now);
         g.gain.linearRampToValueAtTime(0.1, now+att);
         g.gain.linearRampToValueAtTime(0.1, now + dur - dec);
         g.gain.linearRampToValueAtTime(0, now + dur);
+
+
+        o1.connect(g);
+        o2.connect(g);
+        o3.connect(g);
+        g.connect(c.destination)
 
         o1.start(now);
         o1.stop(now+dur);
@@ -232,6 +256,11 @@ function randomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function noteToFreq(note) {
+    let a = 440; //standard frequency of A (common value is 440Hz)
+    return (a / 32) * (2 ** ((note - 9) / 12));
 }
 
 
