0661823410d255340278448698dd34d0c096a5aa
Haokun Song <shk741612898@gmail.com>
2022-02-13 17:11:17 +0100

    Create main.js
    fixed

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-
 //variables used in retrive informations
-const APIKeys = ['a39nt3GG9ZNiFsNmXADD0n4KWPSrlIhf' /*, '7pu6ELCYDhg8YqBTAPNCal6I6svfsuEL'*/];
+const APIKeys = ['fc0JxtUdMY94hZ9lIu9BEiwD5tn2c9jO' /*, '7pu6ELCYDhg8YqBTAPNCal6I6svfsuEL'*/];
 
 // URL of the TILE SERVER
 const url_carto_cdn = 'http://{1-4}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png';
@@ -16,164 +15,142 @@ let decimals = 1;
 /**
  * Click event listener
  */
-document.addEventListener("DOMContentLoaded", function(event)
-{
-    // view
-    var myview = new ol.View({
+
+// view
+var myview = new ol.View({
         center: [1350766.668508934, 5177943.850979362], // map.getView().getCenter()
-        zoom: 6,
+        zoom: 2,
     })
 
-    window.onload = init(); // Call init() when we open the window
-    function init() {
-        const map = new ol.Map({
-            target: 'map', 
-            layers: [
-                new ol.layer.Tile({
-                    source: new ol.source.XYZ({url: url_carto_cdn})
-                })
-            ],
-            view: myview
-        })
-
-        // The following is to create three different layers. openStreetMapStandard, openStreetMapHumanitarian and stamenTerrain
-        const openStreetMapStandard = new ol.layer.Tile({
-            source: new ol.source.OSM(), // open street map
-            visible: true,
-            title: 'OSMStandard'
-        })
-
-        const openStreetMapHumanitarian = new ol.layer.Tile({
-            source: new ol.source.OSM({
-                url: 'http://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
-            }),
-            visible: false,
-            title: 'OSMHumanitarian'
-        })
-
-        const stamenTerrain = new ol.layer.Tile({
-            source: new ol.source.XYZ({
-                url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg',
-                attributions: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
-            }),
-            visible: false,
-            title: 'StamenTerrain'
-        })
-
-        // Layer group
-        const baseLayerGroup = new ol.layer.Group({
-            layers: [
-                openStreetMapStandard, openStreetMapHumanitarian, stamenTerrain
-            ]
-        })
-        map.addLayer(baseLayerGroup);
-
-        // Layer Switcher
-        const baseLayerElements = document.querySelectorAll('.sidebar>input[type=radio]');
-        for (let baseLayerElement of baseLayerElements) {
-            baseLayerElement.addEventListener('change', function () {
-                // console.log(this.value);
-                let baseLayerElementValue = this.value;
-                baseLayerGroup.getLayers().forEach(function (element, index, array) {
-                    let baseLayerTitle = element.get("title");
-                    element.setVisible(baseLayerTitle === baseLayerElementValue);
-                })
-
+window.onload = init(); // Call init() when we open the window
+function init() {
+    const map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.XYZ({url: url_carto_cdn})
             })
-        }
+        ],
+        view: myview
+    })
 
-        // Vector Layers
-        var styles = [
-            new ol.style.Style({
+    // The following is to create three different layers. openStreetMapStandard, openStreetMapHumanitarian and stamenTerrain
+    const openStreetMapStandard = new ol.layer.Tile({
+        source: new ol.source.OSM(), // open street map
+        visible: true,
+        title: 'OSMStandard'
+    })
+
+    const openStreetMapHumanitarian = new ol.layer.Tile({
+        source: new ol.source.OSM({
+            url: 'http://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+        }),
+        visible: false,
+        title: 'OSMHumanitarian'
+    })
+
+    const stamenTerrain = new ol.layer.Tile({
+        source: new ol.source.XYZ({
+            url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg',
+            attributions: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+        }),
+        visible: false,
+        title: 'StamenTerrain'
+    })
+
+    // Layer group
+    const baseLayerGroup = new ol.layer.Group({
+        layers: [
+            openStreetMapStandard, openStreetMapHumanitarian, stamenTerrain
+        ]
+    })
+    map.addLayer(baseLayerGroup);
+
+    // Layer Switcher
+    const baseLayerElements = document.querySelectorAll('.sidebar>input[type=radio]');
+    for (let baseLayerElement of baseLayerElements) {
+        baseLayerElement.addEventListener('change', function () {
+            // console.log(this.value);
+            let baseLayerElementValue = this.value;
+            baseLayerGroup.getLayers().forEach(function (element, index, array) {
+                let baseLayerTitle = element.get("title");
+                element.setVisible(baseLayerTitle === baseLayerElementValue);
+            })
+
+        })
+    }
+
+    // Vector Layers
+    var styles = [
+        new ol.style.Style({
+            fill: new ol.style.Fill({
+                color: [125, 45, 45, 0.15]
+            }),
+            stroke: new ol.style.Stroke({
+                color: 'red', // '#3399CC'
+                width: 1.2
+            }),
+
+            image: new ol.style.Circle({
                 fill: new ol.style.Fill({
-                    color: [125, 45, 45, 0.15]
+                    color: "blue"
                 }),
+                radius: 3.5,
                 stroke: new ol.style.Stroke({
                     color: 'red', // '#3399CC'
                     width: 1.2
                 }),
 
-                image: new ol.style.Circle({
-                    fill: new ol.style.Fill({
-                        color: "blue"
-                    }),
-                    radius: 3.5,
-                    stroke: new ol.style.Stroke({
-                        color: 'red', // '#3399CC'
-                        width: 1.2
-                    }),
-
-                }),
-
-                // image: new ol.style.Icon({ // apply the icon
-                //     scale: 0.04,
-                //     src: 'https://dl.dropboxusercontent.com/u/27798645/fireman/truck65.svg'
-                // }),
-
-                // label: "Capital",
-                // text: new ol.style.Text({
-                //     font: '15px Calibri,sans-serif',
-                //     scale: 1.1,
-                //     fill: new ol.style.Fill({ color: '#000' }),
-                //     stroke: new ol.style.Stroke({
-                //         color: '#fff', //  color: '#FFFF99'
-                //         width: 2
-                //     }),
-                //     text: "Capital",
-                //     offsetY: 18,
-                //     overflow: true
-                //     // placement: 'line'
-                //     // feature.get('name')
-                // })
-
-            })
-        ];
-
-        // Manually created geojson of italy by http://geojson.io/#map=7/51.529/-0.110
-        var ItalyGeoJSON = new ol.layer.VectorImage({
-            source: new ol.source.Vector({
-                format: new ol.format.GeoJSON(),
-                url: './data/vector_data/italyFix.geojson'
             }),
-            visible: true,
-            title: 'ITALY',
-            style: styles
-        })
-        map.addLayer(ItalyGeoJSON);
 
-        // Vector feature popup
-        const overlayContainerElement = document.querySelector(".overlay-container");
-        const overlayLayer = new ol.Overlay({
-            element: overlayContainerElement
         })
-        map.addOverlay(overlayLayer);
-        const overlayFeatureName = document.getElementById('feature-name');
-        const overlayFeatureAdditionalINFO = document.getElementById('feature-additional-info');
+    ];
 
-        map.on('click', function (i) { // create a click event ;
-            overlayLayer.setPosition(undefined);
-            map.forEachFeatureAtPixel(i.pixel, function (feature, layer) {
-                    let clickedCoordinate = i.coordinate;
-                    let lonLatCoordinate = ol.proj.toLonLat(clickedCoordinate) // longitude as 1st and latitude as 2nd element
-                    let clickedFeatureName = feature.get('NAME'); 
-                    let clickedFeatureAdditionalINFO = feature.get('additionalinfo');
-                    overlayLayer.setPosition(clickedCoordinate);
-                    overlayFeatureName.innerHTML = clickedFeatureName;
-                    overlayFeatureAdditionalINFO.innerHTML = clickedFeatureAdditionalINFO;
-                },
-                   {
-                    layerFilter: function (layerCandidate) { // it's a filter that select the geojson you want to use
-                        return layerCandidate.get("title") === 'ITALY'
-                    }
-                })
-        })
+    // Manually created geojson of italy by http://geojson.io/#map=7/51.529/-0.110
+    var ItalyGeoJSON = new ol.layer.VectorImage({
+        source: new ol.source.Vector({
+            format: new ol.format.GeoJSON(),
+            url: './data/vector_data/italyFix.geojson'
+        }),
+        visible: true,
+        title: 'ITALY',
+        style: styles
+    })
+    map.addLayer(ItalyGeoJSON);
 
-        
-        // Create a click event to call getMapCoordOnClick()
-        map.on("click", (e) => getMapCoordOnClick(e));
-               
-    }
-});
+    // Vector feature popup
+    const overlayContainerElement = document.querySelector(".overlay-container");
+    const overlayLayer = new ol.Overlay({
+        element: overlayContainerElement
+    })
+    map.addOverlay(overlayLayer);
+    const overlayFeatureName = document.getElementById('feature-name');
+    const overlayFeatureAdditionalINFO = document.getElementById('feature-additional-info');
+
+    map.on('click', function (i) { // create a click event ;
+        overlayLayer.setPosition(undefined);
+        map.forEachFeatureAtPixel(i.pixel, function (feature, layer) {
+                let clickedCoordinate = i.coordinate;
+                let lonLatCoordinate = ol.proj.toLonLat(clickedCoordinate) // longitude as 1st and latitude as 2nd element
+                let clickedFeatureName = feature.get('NAME');
+                let clickedFeatureAdditionalINFO = feature.get('additionalinfo');
+                overlayLayer.setPosition(clickedCoordinate);
+                overlayFeatureName.innerHTML = clickedFeatureName;
+                overlayFeatureAdditionalINFO.innerHTML = clickedFeatureAdditionalINFO;
+            },
+            {
+                layerFilter: function (layerCandidate) { // it's a filter that select the geojson you want to use
+                    return layerCandidate.get("title") === 'ITALY'
+                }
+            })
+    })
+
+
+    // Create a click event to call getMapCoordOnClick()
+    map.on("click", (e) => getMapCoordOnClick(e));
+
+}
+
 
 /**
  * Get the weather info when click on the map
@@ -188,7 +165,7 @@ const getMapCoordOnClick = (evt) => {
 
     currentCity.longitude = lonlat[0];
     currentCity.latitude = lonlat[1];
-    
+
     // doing the query to get forecast (or load it in current city)
     // Also only represent the city name on the console, can't print it and call it now; It's the problem of async and cannot get the OBJECT correctly
     getCityByCoordinates().then(r => playSound());
@@ -245,11 +222,10 @@ const updateUI = () => {
     //updating image
     let imgSrc = "/data/WeatherIcons/" + weather.iconNumber + ".png";
     image.setAttribute("src", imgSrc);
-    
+
     // zoom in
     const lat = ol.proj.fromLonLat([currentCity.latitude, currentCity.longitude])[1] // -90 , 90
     const lon = ol.proj.fromLonLat([currentCity.latitude, currentCity.longitude])[0] // -180,180
-    // console.log([lat,lon])
 
     if (lon < -180 || lon > 180) {
         Lon = Math.abs(lon + 180, 360) - 180;
@@ -262,7 +238,7 @@ const updateUI = () => {
 }
 
 /**
- * 
+ * Function for zoom in the map to the specific city
  */
 function zoomIn(Lat , Lon)
 {
@@ -273,18 +249,3 @@ function zoomIn(Lat , Lon)
     })
 }
 
-// function zoomtomilano(){
-//     myview.animate({
-//         center: milano,
-//         duration: 1800,
-//         zoom: 11
-//     })
-// }
-
-
-
-// layer
-// var mylayer = new ol.layer.Tile({
-//     source: new ol.source.OSM() // open street map
-// })
-// var layer = [mylayer]
